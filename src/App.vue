39dd03e7cd3db09e3d4045addcc76c5ff43e2cfb
Mauro E. Wernly <wernly.mauro@gmail.com>
2020-02-04 21:00:00 -0300

    start pokegame project to test Vue.js

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pokegame msg=""/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pokegame from './components/Pokegame.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Pokegame
   }
 }
 </script>
---
+++ src/components/Pokegame.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li v-for="pokemon in Pokedex" v-bind:key="pokemon.id">
+        {{pokemon.name.english}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import Pokedex from '../assets/data/pokedex.json'
+
+export default {
+  name: 'Pokegame',
+  props: {
+    msg: String
+  },
+
+  data() {
+    return {
+      Pokedex
+    };
+  },
+  methods: {
+    imagePkmn: function(){
+      pokedex.forEach( pokemon{
+        
+      })
+    };
+},
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
